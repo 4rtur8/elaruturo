684fc1fdb3a9c1b6086acad3f15ed1f1abc0cd79
arturo <arturo8gll@gmail.com>
2021-03-01 15:16:26 -0600

    first commit

---
+++ src/.temp/now.js
@@ -0,0 +1,1 @@
+export default 1614624653655
---
+++ src/.temp/routes.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    path: "/docs/writing-content/",
+    component: () => import(/* webpackChunkName: "page--src--templates--markdown-page-vue" */ "/home/arturo/git/arturo/elaruturo/src/templates/MarkdownPage.vue")
+  },
+  {
+    path: "/docs/sidebar/",
+    component: () => import(/* webpackChunkName: "page--src--templates--markdown-page-vue" */ "/home/arturo/git/arturo/elaruturo/src/templates/MarkdownPage.vue")
+  },
+  {
+    path: "/docs/settings/",
+    component: () => import(/* webpackChunkName: "page--src--templates--markdown-page-vue" */ "/home/arturo/git/arturo/elaruturo/src/templates/MarkdownPage.vue")
+  },
+  {
+    path: "/docs/installation/",
+    component: () => import(/* webpackChunkName: "page--src--templates--markdown-page-vue" */ "/home/arturo/git/arturo/elaruturo/src/templates/MarkdownPage.vue")
+  },
+  {
+    path: "/docs/deploying/",
+    component: () => import(/* webpackChunkName: "page--src--templates--markdown-page-vue" */ "/home/arturo/git/arturo/elaruturo/src/templates/MarkdownPage.vue")
+  },
+  {
+    path: "/docs/",
+    component: () => import(/* webpackChunkName: "page--src--templates--markdown-page-vue" */ "/home/arturo/git/arturo/elaruturo/src/templates/MarkdownPage.vue")
+  },
+  {
+    name: "404",
+    path: "/404/",
+    component: () => import(/* webpackChunkName: "page--src--pages--404-vue" */ "/home/arturo/git/arturo/elaruturo/src/pages/404.vue")
+  },
+  {
+    name: "home",
+    path: "/",
+    component: () => import(/* webpackChunkName: "page--src--pages--index-vue" */ "/home/arturo/git/arturo/elaruturo/src/pages/Index.vue")
+  },
+  {
+    name: "*",
+    path: "*",
+    component: () => import(/* webpackChunkName: "page--src--pages--404-vue" */ "/home/arturo/git/arturo/elaruturo/src/pages/404.vue")
+  }
+]
+
